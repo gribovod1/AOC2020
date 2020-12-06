4e193b6b97eb9c4bea2153581083b0ae67cd9360
gribovod1 <gribovod1@gmail.com>
2020-12-06 08:37:31 +0300

    Отрефакторил решение 2020-6: разделил решения первой и второй задач

--- AOC2020/Day6.cs
+++ AOC2020/Day6.cs
@@ -10,13 +10,40 @@ namespace AOC2020
         public static void exec()
         {
             var groups = File.ReadAllText(@"Data\day6.txt").Split(new string[] {Environment.NewLine + Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
-            var partOne = 0;
+            var one = partOne(groups);
+            var two = partTwo(groups);
 
-            foreach(var g in groups)
+            Console.WriteLine($"partOne: {one} partTwo: {two}");
+            if (Console.ReadKey().Key == ConsoleKey.Spacebar)
+                Clipboard.SetText(two.ToString());
+        }
+
+        public static int partOne(string[] groups)
+        {
+            int result = 0;
+            foreach (var g in groups)
+            {
+                var s = new HashSet<char>();
+                foreach (var c in g)
+                {
+                    if (c >= 'a' && c <= 'z')
+                    {
+                        s.Add(c);
+                    }
+                }
+                result += s.Count;
+            }
+            return result;
+        }
+
+        public static int partTwo(string[] groups)
+        {
+            int result = 0;
+            foreach (var g in groups)
             {
                 HashSet<char> s = null;
                 var first = true;
-                var people = g.Split(new string[] { Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries);
+                var people = g.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
                 foreach (var answers in people)
                 {
                     var current = new HashSet<char>();
@@ -35,14 +62,9 @@ namespace AOC2020
                     else
                         s.IntersectWith(current);
                 }
-                partOne += s.Count;
+                result += s.Count;
             }
-
-            Console.WriteLine($"partOne: {partOne}");
-            if (Console.ReadKey().Key == ConsoleKey.Spacebar)
-            {
-                Clipboard.SetText(partOne.ToString());
-            }
+            return result;
         }
     }
 }
--- AOC2020/Day7.cs
+++ AOC2020/Day7.cs
@@ -13,14 +13,61 @@ namespace AOC2020
         public static void exec()
         {
             var groups = File.ReadAllText(@"Data\day7.txt").Split(new string[] { Environment.NewLine + Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
-            var partOne = 0;
+            var one = partOne(groups);
+            var two = partTwo(groups);
 
-
-            Console.WriteLine($"partOne: {partOne}");
+            Console.WriteLine($"partOne: {one} partTwo: {two}");
             if (Console.ReadKey().Key == ConsoleKey.Spacebar)
+                Clipboard.SetText(two.ToString());
+        }
+
+        public static int partOne(string[] groups)
+        {
+            int result = 0;
+            foreach (var g in groups)
             {
-                Clipboard.SetText(partOne.ToString());
+                var s = new HashSet<char>();
+                foreach (var c in g)
+                {
+                    if (c >= 'a' && c <= 'z')
+                    {
+                        s.Add(c);
+                    }
+                }
+                result += s.Count;
             }
+            return result;
+        }
+
+        public static int partTwo(string[] groups)
+        {
+            int result = 0;
+            foreach (var g in groups)
+            {
+                HashSet<char> s = null;
+                var first = true;
+                var people = g.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (var answers in people)
+                {
+                    var current = new HashSet<char>();
+                    foreach (var c in answers)
+                    {
+                        if (c >= 'a' && c <= 'z')
+                        {
+                            current.Add(c);
+                        }
+                    }
+                    if (first)
+                    {
+                        s = current;
+                        first = false;
+                    }
+                    else
+                        s.IntersectWith(current);
+                }
+                result += s.Count;
+            }
+            return result;
         }
     }
 }
