98759a541e5c3ae5003cf07fbf2da1f197c2cf1a
Sergey Shilin <gribovod1@gmail.com>
2020-12-07 09:56:28 +0300

    Добавлено решение 2020-7

--- AOC2020/Day7.cs
+++ AOC2020/Day7.cs
@@ -2,9 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace AOC2020
 {
@@ -12,62 +10,93 @@ namespace AOC2020
     {
         public static void exec()
         {
-            var groups = File.ReadAllText(@"Data\day7.txt").Split(new string[] { Environment.NewLine + Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
-            var one = partOne(groups);
-            var two = partTwo(groups);
+            var rules = File.ReadAllLines(@"Data\day7.txt");
+            var one = partOne(rules);
+            var two = partTwo(rules);
 
             Console.WriteLine($"partOne: {one} partTwo: {two}");
             if (Console.ReadKey().Key == ConsoleKey.Spacebar)
                 Clipboard.SetText(two.ToString());
         }
 
-        public static int partOne(string[] groups)
+        public static int partOne(string[] rules)
         {
-            int result = 0;
-            foreach (var g in groups)
+            var s = new HashSet<string>();
+            var containBags = new HashSet<string>();
+            containBags.Add("shiny gold");
+            var added = 0;
+            do
             {
-                var s = new HashSet<char>();
-                foreach (var c in g)
+                added = 0;
+                foreach (var r in rules)
                 {
-                    if (c >= 'a' && c <= 'z')
+                    var bags = Regex.Replace(r, "[0-9]", "", RegexOptions.IgnoreCase).Split(new string[] { ",", " contain ", ".", " bags", " bag" }, StringSplitOptions.RemoveEmptyEntries);
+                    foreach (var b in bags)
                     {
-                        s.Add(c);
-                    }
-                }
-                result += s.Count;
-            }
-            return result;
-        }
-
-        public static int partTwo(string[] groups)
-        {
-            int result = 0;
-            foreach (var g in groups)
-            {
-                HashSet<char> s = null;
-                var first = true;
-                var people = g.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
-                foreach (var answers in people)
-                {
-                    var current = new HashSet<char>();
-                    foreach (var c in answers)
-                    {
-                        if (c >= 'a' && c <= 'z')
+                        var t = b.Trim();
+                        if (containBags.Contains(t))
                         {
-                            current.Add(c);
+                            if (s.Add(bags[0]))
+                            {
+                                containBags.Add(bags[0]);
+                                added++;
+                            }
                         }
                     }
-                    if (first)
-                    {
-                        s = current;
-                        first = false;
-                    }
-                    else
-                        s.IntersectWith(current);
                 }
-                result += s.Count;
+            } while (added > 0);
+            s.Remove("shiny gold");
+            return s.Count;
+        }
+
+        class Bag
+        {
+            List<KeyValuePair<string, int>> Bags;
+
+            public Bag(string containBags)
+            {
+                Bags = new List<KeyValuePair<string, int>>();
+                var bags = containBags.Split(new string[] { ",", " contain ", ".", " bags", " bag" }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (var b in bags)
+                {
+                    var numBag = b.Trim();
+                    var index = numBag.IndexOf(" ");
+                    if (int.TryParse(numBag.Substring(0, index), out int count))
+                    {
+                        var bag = numBag.Substring(index + 1, numBag.Length - index - 1);
+                        Bags.Add(new KeyValuePair<string, int>(bag, count));
+                    }
+                }
             }
-            return result;
+
+            public int Summary(Dictionary<string, Bag> bags)
+            {
+                int result = 0;
+                foreach (var b in Bags)
+                {
+                    result += (bags[b.Key].Summary(bags) + 1) * b.Value;
+                }
+                return result;
+            }
+        }
+
+        public static int partTwo(string[] rules)
+        {
+            var bags = new Dictionary<string, Bag>();
+            Bag sgold = null;
+            foreach (var r in rules)
+            {
+                var ss = " bags contain";
+                var startIndex = r.IndexOf(ss);
+                if (startIndex > 0)
+                {
+                    var b = new Bag(r.Substring(startIndex + ss.Length, r.Length - startIndex - ss.Length));
+                    bags.Add(r.Substring(0, startIndex), b);
+                    if (r.IndexOf("shiny gold") == 0)
+                        sgold = b;
+                }
+            }
+            return sgold.Summary(bags);
         }
     }
 }
