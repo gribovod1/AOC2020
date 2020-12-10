5676a409f0c9ddb9006f0c516b4fb0bc36e12596
gribovod1 <gribovod1@gmail.com>
2020-12-10 21:11:06 +0300

    2020-10: Чуть-чуть сокращений и переименований. Ну типа рефакторинг.

--- AOC2020/Day10.cs
+++ AOC2020/Day10.cs
@@ -11,57 +11,43 @@ namespace AOC2020
         public static void Exec()
         {
             var numbersText = File.ReadAllLines(@"Data\day10.txt");
-            var numbers = new List<ulong>();
+            var numberList = new List<ulong>() { 0 };
             foreach (var s in numbersText)
-                numbers.Add(ulong.Parse(s));
+                numberList.Add(ulong.Parse(s));
+            numberList.Sort();
+            numberList.Add(numberList[numberList.Count - 1] + 3);
+            var numbers = numberList.ToArray();
             var one = partOne(numbers);
-            var two = partTwo(numbers);
+            var two = countExcludeVariations(numbers, 1);
 
             Console.WriteLine($"partOne: {one} partTwo: {two}");
             if (Console.ReadKey().Key == ConsoleKey.Spacebar)
                 Clipboard.SetText(two.ToString());
         }
 
-        public static int partOne(List<ulong> numbers)
+        public static int partOne(ulong[] numbers)
         {
-            numbers.Sort();
             var count1 = 0;
-            var count3 = 1;
-            if (numbers[0] == 1)
-                ++count1;
-            if (numbers[0] == 3)
-                ++count3;
-            for (int i = 1; i < numbers.Count; ++i)
+            var count3 = 0;
+            for (int i = 1; i < numbers.Length; ++i)
             {
                 if (numbers[i] - numbers[i - 1] == 1)
                     ++count1;
                 if (numbers[i] - numbers[i - 1] == 3)
                     ++count3;
             }
-            Console.WriteLine($"count1 +count3: {count1 + count3} numbers.Count: {numbers.Count}");
             return count1 * count3;
         }
 
-        public static ulong partTwo(List<ulong> numbers)
-        {
-            numbers.Add(0);
-            numbers.Sort();
-            numbers.Add(numbers[numbers.Count - 1] + 3);
-            return calc(numbers.ToArray(), 1);
-        }
-
-        static ulong calc(ulong[] numbers, int index)
+        static ulong countExcludeVariations(ulong[] numbers, int index)
         {
             if (index == numbers.Length - 1)
                 return 1;
-            if (numbers[index + 1] - numbers[index - 1] <= 3)
-            {
-                var endExclude = findEndIndex(numbers, index);
-                ulong varCount = calcBrute(numbers, index, endExclude);
-                return varCount * calc(numbers, endExclude);
-            }
-            else
-                return calc(numbers, index + 1);
+            if (numbers[index + 1] - numbers[index - 1] > 3)
+                return countExcludeVariations(numbers, index + 1);
+            var endExclude = findEndIndex(numbers, index);
+            ulong localVariationsCount = calcBrute(numbers, index, endExclude);
+            return localVariationsCount * countExcludeVariations(numbers, endExclude);
         }
 
         static int findEndIndex(ulong[] numbers, int index)
@@ -81,11 +67,11 @@ namespace AOC2020
                 return calcBrute(numbers, index + 1, endIndex);
         }
 
-        static ulong[] excludeClone(ulong[] numbers, int index)
+        static ulong[] excludeClone(ulong[] numbers, int excludeIndex)
         {
             var result = new ulong[numbers.Length - 1];
             for (var i = 0; i < result.Length; ++i)
-                result[i] = i < index ? numbers[i] : numbers[i + 1];
+                result[i] = i < excludeIndex ? numbers[i] : numbers[i + 1];
             return result;
         }
     }
